489b452388e872261750648fb10274e0332c9c48
Abhishek Yadav <abhishekyadav262005@gmail.com>
2024-10-11 15:11:37 +0530

    added movement and gravity concept
    added movement and gravity

--- js fils/app.js
+++ js fils/app.js
@@ -4,6 +4,9 @@ const c = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
+
+const gravity =1 ;
+
 class Player {
     constructor() {
       this.position = {
@@ -14,15 +17,38 @@ class Player {
          x: 0,
          y:0
          }
-      this.width = 100;
-      this.height = 100;
+      this.width = 30;
+      this.height = 30;
  
     }
 
     draw(){
         c.fillRect(this.position.x,this.position.y,this.width,this.height)
     }
+
+    update(){
+  this.draw();
+  this.position.y +=this.velocity.y
+  
+  if(this.position.y + this.height + this.velocity.y <=canvas.height)
+  this.velocity.y +=gravity ;
+
+  else
+  this.velocity.y =0 ;
+   
+
+    }
+
+}
+const player=new Player();
+player.update();
+
+function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0,0,canvas.width,canvas.height);
+    player.update();
 }
 
-const player=new Player();
-player.draw();
+animate();
+
+addEventListener("keydown");
